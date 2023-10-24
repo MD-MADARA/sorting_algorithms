d3d72a5f4860942674b97cb40f0e0d1b417f279f
MD-MADARA <mouadotaku9421@gmail.com>
2023-10-24 21:13:53 +0100

    cocktail_sort + counting sort

--- sort.h
+++ sort.h
@@ -2,7 +2,6 @@
 #define SORT_H
 
 #include <stdlib.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -17,17 +16,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-void bubble_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
 void quick_sort(int *array, size_t size);
-void print_sort(int array[], size_t size, int init);
-size_t partition(int array[], size_t size);
-void swap(int *a, int *b);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
